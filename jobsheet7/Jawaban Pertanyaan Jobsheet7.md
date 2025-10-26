61fcad2fc65041b3561bd4bd749eadeb4bcf9fee
fahrulmuzaki06 <fahrulmuzaki06@gmail.com>
2025-10-26 15:45:19 +0700

    Add files via upload

---
+++ jobsheet7/Jawaban Pertanyaan Jobsheet7.md
@@ -0,0 +1,10 @@
+Jawaban  
+
+Pertanyaan 2.1.3
+1.  inisialisasi    : int i = 1
+    kondisi         : i <= 10
+    update          : i++
+2.  Supaya bisa mengasumsikan nilai mahasiswa di rentang 0 sampai 100
+    Jika dibalik maka nilai tertinggi tidak akan berubah karena tidak ada nilai lebih dari 100 dan nilai terendah tetap 0 karena tidak ada nilai kurang dari 0.
+3.  Untuk mencari nilai tertinggi dan terendah
+    Alur kerja : program membandingkan nilai baru dengan nilai tertinggi/terendah sementara kalau nilai baru lebih besar/lebih kecil maka variabel nilai tertinggi/terendah akan diganti dengan nilai itu.
---
+++ jobsheet7/siakadfor14.java
@@ -0,0 +1,32 @@
+package jobsheet7;
+import java.util.Scanner;
+public class siakadfor14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi){
+                tertinggi = nilai;
+            }
+            if (nilai < terendah){
+                terendah = nilai;
+
+            }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
+        }
+    System.out.println("Nilai Tertinggi     : " + tertinggi);
+    System.out.println("Nilai Terendah      :" + terendah);
+    System.out.println("Jumlah Lulus        :" + lulus);
+    System.out.println("Jumlah Tidak Lulus  :" + tidakLulus);
+    sc.close();
+    
+    }
+}
